983fc86d90d2bc02dc71c67c614243367a23ae24
谈超 <tanchao@mobike.com>
2017-09-18 00:23:51 +0800

    初始化

---
+++ ImpulseAnimation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImpulseAnimation
+//
+//  Created by 谈超 on 2017/9/17.
+//  Copyright © 2017年 谈超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let animationView = ImpulseAnimationView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: 120, height: 120)))
+        animationView.center = view.center
+        view.addSubview(animationView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
